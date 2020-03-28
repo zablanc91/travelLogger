b12fa435c1fb1f2c0b5d9ab56785d0dbdf3f642d
Carlo Zablan <zablanc91@hotmail.com>
2020-03-28 16:19:53 -0700

    started on authentication with Google, able to retrieve user info

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hello: 'how low?'});
-});
+//tell passport how to use our Strategy, 2nd argument to Google Strategy is arrow funct as callback
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (profileAccessToken, refreshToken, profile, done) => {
+        //only concerned with profile information, specifically id for our DB
+        console.log('profile info: ', profile);
+    })
+);
+
+//test route
+app.get(
+    '/', 
+    (req, res) => {
+        res.send({hello: 'how low?'});
+    }
+);
+
+//route to start passport flow
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {scope: ['profile', 'email']})
+);
+
+//route for auth callback, will run arrow funct in passport's 2nd arg above
+app.get(
+    '/auth/google/callback', 
+    passport.authenticate('google')
+);
 
 app.listen(2020);
