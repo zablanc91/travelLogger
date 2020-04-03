cd3fa508393bb27ebb0a9e9b3c185a8b2e921d81
Carlo Zablan <zablanc91@hotmail.com>
2020-04-02 19:01:10 -0700

    started working on LogEntry model and AddEntry component form

--- client/src/components/AddLog.js
+++ client/src/components/AddLog.js
@@ -1,11 +1,66 @@
 import React from 'react';
 
-const Landing = () => {
-    return (
-        <div>
-            Landing for now.
+const AddLog = () => {
+    return (      
+        <div class="row">
+            <h3 class="center-align">New Entry</h3>
+            <form class="col s12" action='#'>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="name" type="text" />
+                        <label class="active" for="name">First Name</label>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="description" type="text" />
+                        <label class="active" for="name">Description</label>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <label>Upload Image</label>
+                    <div class="file-field input-field">
+                        <div class="btn">
+                            <span>File</span>
+                            <input type="file" />
+                        </div>
+                        <div class="file-path-wrapper">
+                            <input class="validate file-path" type="text" placeholder="Upload file"/>
+                        </div>
+                    </div>
+                </div>
+
+                 <div class="row">
+                    <div class="input-field col s12">
+                        <input id="address" type="text" />
+                        <label class="active" for="name">Address</label>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="input-field col s12">
+                        <input id="address" type="text" />
+                        <label class="active" for="name">Address</label>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="input-field col s6">
+                        <input id="lat" type="text" />
+                        <label class="active" for="name">Latitude</label>
+                    </div>
+                    <div class="input-field col s6">
+                        <input id="lng" type="text" />
+                        <label class="active" for="name">Longitude</label>
+                    </div>
+                </div>
+
+            </form>
         </div>
+      
     );
 };
 
-export default Landing;
+export default AddLog;
---
+++ models/LogEntry.js
@@ -0,0 +1,30 @@
+//Model Class for the LogEntry Collection in MongoDB
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const logEntrySchema = new Schema({
+    name: {
+        type: String,
+        required: 'Please enter a name for the entry.'
+    },
+    description: String,
+    location: {
+        type: {
+            type: String,
+            default: 'Point'
+        },
+        //lng then lat inside coordinates array
+        coordinates: [{
+            type: Number, 
+            required: 'You must supply coordinates.'
+        }],
+        address: {
+            type: String,
+            required: 'You must supply an address.'
+        }
+    },
+    photo: String
+});
+
+mongoose.model('logEntries', logEntrySchema);
