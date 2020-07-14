ef8874f960230665d7bafa2a76e7663e116a5e8f
Carlo Zablan <zablanc91@hotmail.com>
2020-07-14 14:33:51 -0700

    env vars for heroku

--- client/src/config/prod.js
+++ client/src/config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-    placesAPI: process.env.PLACES_API,
-    mapsAPI: process.env.MAPS_API,
-    mapsAPIToken: process.env.MAPS_API_TOKEN,
-    mapboxToken: process.env.MAPBOX_TOKEN
+    placesAPI: process.env.REACT_APP_PLACES_API,
+    mapsAPI: process.env.REACT_APP_MAPS_API,
+    mapsAPIToken: process.env.REACT_APP_MAPS_API_TOKEN,
+    mapboxToken: process.env.REACT_APP_MAPBOX_TOKEN
 }
